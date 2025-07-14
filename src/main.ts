c822635a76b1d531bd24201bf0a8fcf6442bea10
mariana soares <marianasoares1240@gmail.com>
2025-07-14 11:52:06 +0000

    main

--- src/main.ts
+++ src/main.ts
@@ -5,39 +5,49 @@
 
 // Requisitos da entrega:
 // 1. Criar um array de números. ✅
-// 2. Criar uma função para exibir o array e seu tamanho. ❌
-// 3. Adicionar elementos 60 e 70 ao array. ❌
-// 4. Exibir o array usando a função mostrarArray. ❌
-// 5. Remover o último elemento do array usando o método pop. ❌
-// 6. Exibir o array usando a função mostrarArray. ❌
-// 7. Mostrar o elemento excluído. ❌
-// 8. Mostrar cada elemento do array em uma linha usando o método forEach. ❌
-// 9. Criar um novo array com os valores dobrados usando o método map. ❌
-// 10. Calcular a soma de todos os elementos do array usando o método reduce. ❌
+// 2. Criar uma função para exibir o array e seu tamanho. ✅
+// 3. Adicionar elementos 60 e 70 ao array. ✅
+// 4. Exibir o array usando a função mostrarArray. ✅
+// 5. Remover o último elemento do array usando o método pop. ✅
+// 6. Exibir o array usando a função mostrarArray. ✅
+// 7. Mostrar o elemento excluído. ✅
+// 8. Mostrar cada elemento do array em uma linha usando o método forEach. ✅
+// 9. Criar um novo array com os valores dobrados usando o método map. ✅
+// 10. Calcular a soma de todos os elementos do array usando o método reduce. ✅
 
 const numeros: number[] = [10, 20, 30, 40, 50];
 
+//2
 function mostrarArray(array: number[]): void {
     console.log('--------------------------------');
-    console.log(`Tamanho: `);
-    console.log(`Array: `);
+    console.log(`Tamanho: ${array.length}`);
+    console.log(`Array: ${array}`);
 }
 mostrarArray(numeros);
 
+//3
+numeros.push(60, 70);
 
+//5
+const elementoExcluido = numeros.pop();
 
-
-
-const elementoExcluido = ;
+//6
 mostrarArray(numeros);
+
+//7
 console.log(`Elemento excluído: ${elementoExcluido}`);
 
-numeros.forEach();
+//8
+numeros.forEach((num) => {
+    console.log(num);
+});
 
-const dobrados = numeros.map();
+//9
+const dobrados = numeros.map((num) => num * 2);
 mostrarArray(dobrados);
 
-const soma = numeros.reduce();
+//10
+const soma = numeros.reduce((acum, atual) => acum + atual, 0);
 console.log(`Soma dos elementos do array: ${soma}`);
 
 // para rodar o código, use o comando:
